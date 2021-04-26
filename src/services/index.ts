63682a87da607de20a17b54243c806d563151bf8
Vladzur <vladzur@gmail.com>
2021-04-26 11:45:08 -0400

    Fix line

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,4 +1,5 @@
 export * from './apichile.service';
 export * from './google-cal.service';
 export * from './api-chile-tranformer.service';
-export * from './google-api-transformer.service';export * from './app-key.service';
+export * from './google-api-transformer.service';
+export * from './app-key.service';
