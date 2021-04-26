9e9321f4c73d75c59ece86ff59ceaf279cdbfc74
greinnyc <greinny@gmail.com>
2021-04-26 18:17:00 -0500

    indentaciÃon en user-credentials.model

--- src/models/user-credentials.model.ts
+++ src/models/user-credentials.model.ts
@@ -2,34 +2,34 @@ import { Entity, model, property } from '@loopback/repository';
 
 @model()
 export class UserCredentials extends Entity {
-    @property({
-        type: 'string',
-        id: true,
-        mongodb: { dataType: 'ObjectID' },
-    })
-    id: string;
+  @property({
+    type: 'string',
+    id: true,
+    mongodb: { dataType: 'ObjectID' },
+  })
+  id: string;
 
-    @property({
-        type: 'string',
-        required: true,
-    })
-    password: string;
+  @property({
+    type: 'string',
+    required: true,
+  })
+  password: string;
 
-    @property({
-        type: 'string',
-        required: true,
-        mongodb: { dataType: 'ObjectID' },
-    })
-    userId: string;
+  @property({
+    type: 'string',
+    required: true,
+    mongodb: { dataType: 'ObjectID' },
+  })
+  userId: string;
 
-    constructor(data?: Partial<UserCredentials>) {
-        super(data);
-    }
+  constructor(data?: Partial<UserCredentials>) {
+    super(data);
+  }
 }
 
 export interface UserCredentialsRelations {
-    // describe navigational properties here
+  // describe navigational properties here
 }
 
 export type UserCredentialsWithRelations = UserCredentials &
-    UserCredentialsRelations;
+  UserCredentialsRelations;
