806b2b065ca0e43f39975de50128f62044e09489
lcubas <langelcubas9@gmail.com>
2021-08-04 11:36:01 -0500

    add report coverage

--- src/__tests__/helpers/database.helpers.ts
+++ src/__tests__/helpers/database.helpers.ts
@@ -1,4 +1,4 @@
-import { Applications } from '../../models';
+import { Applications, User, UserCredentials } from '../../models';
 
 export function givenApplicationData(application?: Partial<Applications>) {
   const data = Object.assign({
@@ -9,4 +9,27 @@ export function givenApplicationData(application?: Partial<Applications>) {
   );
 
   return new Applications(data);
+}
+
+export function givenUserData(user?: Partial<User>) {
+  const data = Object.assign({
+      id: 'mogo-id-obj',
+      email: 'admin@email.com',
+    },
+    user,
+  );
+
+  return new User(data);
+}
+
+export function givenUserCredentialsData(user?: Partial<UserCredentials>) {
+  const data = Object.assign({
+      id: 'mogo-id-obj-2',
+      password: 'secure-password',
+      userId: 'mogo-id-obj',
+    },
+    user,
+  );
+
+  return new UserCredentials(data);
 }
